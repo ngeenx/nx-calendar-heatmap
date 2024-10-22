a4f012c3655afd9d502f51f48d36903db8ec1e0e
relliv <epsilonelittr@gmail.com>
2024-10-22 13:29:43 +0300

    Apply formatting

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,47 +1,53 @@
 <template>
   <div class="flex flex-col items-start justify-start gap-5">
     <span>
-      {{startDate}}
+      {{ startDate }}
     </span>
 
-    <NxCalendarHeatmap :options="{
-      type: 'yearly',
-      startDate: startDate,
-      cellSize: cellSize,
-      hideEmptyDays: false,
-      onClick: onDayClick,
-    }"/>
+    <NxCalendarHeatmap
+      :options="{
+        type: 'yearly',
+        startDate: startDate,
+        cellSize: cellSize,
+        hideEmptyDays: false,
+        onClick: onDayClick,
+      }"
+    />
 
     <div class="flex flex-row">
-      <NxCalendarHeatmap :options="{
-        type: 'monthly',
-        startDate: month,
-        cellSize: cellSize,
-        onClick: onDayClick
-      }" v-for="month in months"/>
+      <NxCalendarHeatmap
+        v-for="(month, index) in months"
+        :key="index"
+        :options="{
+          type: 'monthly',
+          startDate: month,
+          cellSize: cellSize,
+          onClick: onDayClick,
+        }"
+      />
     </div>
 
-    <NxCalendarHeatmap :options="{
-      type: 'weekly',
-      startDate: startDate,
-      cellSize: cellSize,
-      onClick: onDayClick
-    }"/>
+    <NxCalendarHeatmap
+      :options="{
+        type: 'weekly',
+        startDate: startDate,
+        cellSize: cellSize,
+        onClick: onDayClick,
+      }"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { NxCalendarHeatmap } from '@ngeenx/nx-vue-calendar-heatmap';
-import { IHeatmapDay } from "@ngeenx/nx-calendar-heatmap-utils";
-import { DateTime } from "luxon";
-import { ref } from "vue";
+import { IHeatmapDay } from '@ngeenx/nx-calendar-heatmap-utils';
+import { DateTime } from 'luxon';
+import { ref } from 'vue';
 
-const startDate = ref("1998-01-01");
+const startDate = ref('1998-01-01');
 const cellSize = 15;
 
-const heatmapColors = [
-  { min: 0, max: 0, color: "#FFFFFF" },
-]
+const heatmapColors = [{ min: 0, max: 0, color: '#FFFFFF' }];
 
 const months = Array.from({ length: 12 }, (_, i) => {
   const firstDayOfMonth = DateTime.local().set({ month: i + 1, day: 1 });
@@ -50,7 +56,7 @@ const months = Array.from({ length: 12 }, (_, i) => {
 
 const onDayClick = (day: IHeatmapDay) => {
   console.log(`Clicked on ${day.date} with value ${day.count}`);
-}
+};
 </script>
 
 <style scoped lang="scss">
