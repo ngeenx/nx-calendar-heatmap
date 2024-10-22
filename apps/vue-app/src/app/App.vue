e3b15c3753f79782e747f1dbdeda366d7eb95a4c
relliv <epsilonelittr@gmail.com>
2024-10-22 13:25:02 +0300

    Add demo layout

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,44 +1,58 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-</script>
-
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-  <RouterView />
+  <div class="flex flex-col items-start justify-start gap-5">
+    <span>
+      {{startDate}}
+    </span>
+
+    <NxCalendarHeatmap :options="{
+      type: 'yearly',
+      startDate: startDate,
+      cellSize: cellSize,
+      hideEmptyDays: false,
+      onClick: onDayClick,
+    }"/>
+
+    <div class="flex flex-row">
+      <NxCalendarHeatmap :options="{
+        type: 'monthly',
+        startDate: month,
+        cellSize: cellSize,
+        onClick: onDayClick
+      }" v-for="month in months"/>
+    </div>
+
+    <NxCalendarHeatmap :options="{
+      type: 'weekly',
+      startDate: startDate,
+      cellSize: cellSize,
+      onClick: onDayClick
+    }"/>
+  </div>
 </template>
 
+<script setup lang="ts">
+import { NxCalendarHeatmap } from '@ngeenx/nx-vue-calendar-heatmap';
+import { IHeatmapDay } from "@ngeenx/nx-calendar-heatmap-utils";
+import { DateTime } from "luxon";
+import { ref } from "vue";
+
+const startDate = ref("1998-01-01");
+const cellSize = 15;
+
+const heatmapColors = [
+  { min: 0, max: 0, color: "#FFFFFF" },
+]
+
+const months = Array.from({ length: 12 }, (_, i) => {
+  const firstDayOfMonth = DateTime.local().set({ month: i + 1, day: 1 });
+  return firstDayOfMonth.toISODate();
+});
+
+const onDayClick = (day: IHeatmapDay) => {
+  console.log(`Clicked on ${day.date} with value ${day.count}`);
+}
+</script>
+
 <style scoped lang="scss">
-header {
-  line-height: 1.5;
-  max-width: 100vw;
-}
-
-nav > a {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-@media (min-width: 768px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 768px;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+@import '../../../../libs/utils/nx-calendar-heatmap-utils/src/styles/app.component.scss';
 </style>
