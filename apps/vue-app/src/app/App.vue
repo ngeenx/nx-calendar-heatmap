cd6b513eb899389887d82056ee31b030d36d196c
relliv <epsilonelittr@gmail.com>
2024-10-22 14:10:49 +0300

    Add year selection to demo app

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,44 +1,55 @@
 <template>
-  <div class="flex flex-col items-start justify-start p-12 gap-7">
-    <span> Yearly </span>
+  <div class="flex flex-col">
+    <select
+      v-model="selectedYear"
+      class="p-2 m-2 border border-gray-300 rounded-md"
+    >
+      <option v-for="(year, index) in years" :key="index">
+        {{ year }}
+      </option>
+    </select>
 
-    <NxCalendarHeatmap
-      :options="{
-        type: HeatMapCalendarType.YEARLY,
-        startDate: startDate,
-        cellSize: cellSize,
-        hideEmptyDays: false,
-        onClick: onDayClick,
-      }"
-      class="p-4 bg-white rounded-md"
-    />
+    <div class="flex flex-col items-start justify-start gap-3 p-5">
+      <span> Yearly </span>
 
-    <span> Monthly </span>
-
-    <div class="flex flex-row p-4 bg-white rounded-md gap-x-2">
       <NxCalendarHeatmap
-        v-for="(month, index) in months"
-        :key="index"
         :options="{
-          type: HeatMapCalendarType.MONTHLY,
-          startDate: month,
+          type: HeatMapCalendarType.YEARLY,
+          startDate: startDate,
+          cellSize: cellSize,
+          hideEmptyDays: false,
+          onClick: onDayClick,
+        }"
+        class="p-4 bg-white rounded-md"
+      />
+
+      <span> Monthly </span>
+
+      <div class="flex flex-row p-4 bg-white rounded-md gap-x-2">
+        <NxCalendarHeatmap
+          v-for="(month, index) in months"
+          :key="index"
+          :options="{
+            type: HeatMapCalendarType.MONTHLY,
+            startDate: month,
+            cellSize: cellSize,
+            onClick: onDayClick,
+          }"
+        />
+      </div>
+
+      <span> Weekly </span>
+
+      <NxCalendarHeatmap
+        :options="{
+          type: HeatMapCalendarType.WEEKLY,
+          startDate: startDate,
           cellSize: cellSize,
           onClick: onDayClick,
         }"
+        class="p-4 bg-white rounded-md"
       />
     </div>
-
-    <span> Weekly </span>
-
-    <NxCalendarHeatmap
-      :options="{
-        type: HeatMapCalendarType.WEEKLY,
-        startDate: startDate,
-        cellSize: cellSize,
-        onClick: onDayClick,
-      }"
-      class="p-4 bg-white rounded-md"
-    />
   </div>
 </template>
 
@@ -49,7 +60,7 @@ import {
   HeatMapCalendarType,
 } from '@ngeenx/nx-calendar-heatmap-utils';
 import { DateTime } from 'luxon';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const startDate = ref('1998-01-01');
 const cellSize = 15;
@@ -61,9 +72,17 @@ const months = Array.from({ length: 12 }, (_, i) => {
   return firstDayOfMonth.toISODate();
 });
 
+const years = ref(Array.from({ length: 30 }, (_, i) => i + 1998).reverse());
+
 const onDayClick = (day: IHeatmapDay) => {
   console.log(`Clicked on ${day.date} with value ${day.count}`);
 };
+
+const selectedYear = ref(years.value[0]);
+
+watch(selectedYear, () => {
+  startDate.value = `${selectedYear.value}-01-01`;
+});
 </script>
 
 <style scoped lang="scss">
