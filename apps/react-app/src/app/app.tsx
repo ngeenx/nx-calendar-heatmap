ffac5974314d8d498a8330f7743e0c26e275b578
relliv <epsilonelittr@gmail.com>
2024-10-26 02:59:15 +0300

    Remove welcome component

--- apps/react-app/src/app/app.tsx
+++ apps/react-app/src/app/app.tsx
@@ -1,15 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-app" />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
