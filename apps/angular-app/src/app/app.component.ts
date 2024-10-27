9faa36beeb703deb815ed61ebe752248a7bedadc
relliv <epsilonelittr@gmail.com>
2024-10-27 11:10:37 +0300

    Use global app styles

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [ RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterModule],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "/libs/utils/nx-calendar-heatmap-utils/src/styles/app.component.scss",
+  ],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = "angular-app";
 }
