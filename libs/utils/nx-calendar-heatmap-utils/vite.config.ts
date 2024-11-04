89f8af487445d2cd4ce0a8d739134f3cc62f7f29
relliv <epsilonelittr@gmail.com>
2024-11-04 21:47:37 +0300

    Fix bundle size issue

--- libs/utils/nx-calendar-heatmap-utils/vite.config.ts
+++ libs/utils/nx-calendar-heatmap-utils/vite.config.ts
@@ -1,19 +1,19 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import * as path from "path";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../../node_modules/.vite/libs/utils/nx-calendar-heatmap-utils',
+  cacheDir: "../../../node_modules/.vite/libs/utils/nx-calendar-heatmap-utils",
   plugins: [
     nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
+    nxCopyAssetsPlugin(["*.md"]),
     dts({
-      entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      entryRoot: "src",
+      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
     }),
   ],
   // Uncomment this if you are using workers.
@@ -23,7 +23,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../../dist/libs/utils/nx-calendar-heatmap-utils',
+    outDir: "../../../dist/libs/utils/nx-calendar-heatmap-utils",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -31,28 +31,36 @@ export default defineConfig({
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'nx-calendar-heatmap-utils',
-      fileName: 'index',
+      entry: "src/index.ts",
+      name: "nx-calendar-heatmap-utils",
+      fileName: "index",
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: [],
+      external: ["tippy.js", "luxon"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          "tippy.js": "tippy",
+          luxon: "luxon",
+        },
+      },
     },
   },
   test: {
     watch: false,
     globals: true,
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    reporters: ["default"],
     coverage: {
       reportsDirectory:
-        '../../../coverage/libs/utils/nx-calendar-heatmap-utils',
-      provider: 'v8',
+        "../../../coverage/libs/utils/nx-calendar-heatmap-utils",
+      provider: "v8",
     },
   },
 });
