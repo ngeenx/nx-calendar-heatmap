e4eeee5866c5802707d1a03c9654b02d50d0b242
relliv <epsilonelittr@gmail.com>
2024-11-05 16:06:14 +0300

    Init react docs

--- libs/features/nx-react-calendar-heatmap/vite.config.ts
+++ libs/features/nx-react-calendar-heatmap/vite.config.ts
@@ -1,22 +1,22 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import * as path from "path";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 
 export default defineConfig({
   root: __dirname,
   cacheDir:
-    '../../../node_modules/.vite/libs/features/nx-react-calendar-heatmap',
+    "../../../node_modules/.vite/libs/features/nx-react-calendar-heatmap",
   plugins: [
     react(),
     nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
+    nxCopyAssetsPlugin(["*.md"]),
     dts({
-      entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      entryRoot: "src",
+      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
     }),
   ],
   // Uncomment this if you are using workers.
@@ -26,7 +26,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../../dist/libs/features/nx-react-calendar-heatmap',
+    outDir: "../../../dist/libs/features/nx-react-calendar-heatmap",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -34,28 +34,34 @@ export default defineConfig({
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'nx-react-calendar-heatmap',
-      fileName: 'index',
+      entry: "src/index.ts",
+      name: "nx-react-calendar-heatmap",
+      fileName: "index",
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "tippy.js",
+        "luxon",
+      ],
     },
   },
   test: {
     watch: false,
     globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    reporters: ["default"],
     coverage: {
       reportsDirectory:
-        '../../../coverage/libs/features/nx-react-calendar-heatmap',
-      provider: 'v8',
+        "../../../coverage/libs/features/nx-react-calendar-heatmap",
+      provider: "v8",
     },
   },
 });
